c4dd5ad6b5d1eb9bff265f275089adbb4d7dd4db
warlordlizard <alexjbowers88@gmail.com>
2018-02-28 15:02:34 -0800

    connected to mongo

--- lab-alex/server.js
+++ lab-alex/server.js
@@ -9,7 +9,13 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const MOGODB_URI = 'mongodb://localhost/gamesdb';
 
+mongoose.connect(MOGODB_URI);
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(errors);
 
 app.listen(PORT, () => {
   debug(`listening on ${PORT}`);
